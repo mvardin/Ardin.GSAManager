d23a010bea85c90361117ea7b0a0404e61733a3a
Ardin <Ardin@gmail.com>
2022-02-17 08:36:11 +0330

    add cb download and ....

--- Ardin.GSAManager/frmMain.Designer.cs
+++ Ardin.GSAManager/frmMain.Designer.cs
@@ -31,8 +31,12 @@ namespace Ardin.GSAManager
         {
             this.btnStart = new System.Windows.Forms.Button();
             this.lbLog = new System.Windows.Forms.ListBox();
-            this.lblStatus = new System.Windows.Forms.Label();
+            this.aaa = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.lblSerStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCbStatus = new System.Windows.Forms.Label();
+            this.cbCBDownload = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // btnStart
@@ -52,19 +56,19 @@ namespace Ardin.GSAManager
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lbLog.FormattingEnabled = true;
             this.lbLog.ItemHeight = 15;
-            this.lbLog.Location = new System.Drawing.Point(12, 41);
+            this.lbLog.Location = new System.Drawing.Point(12, 71);
             this.lbLog.Name = "lbLog";
-            this.lbLog.Size = new System.Drawing.Size(776, 394);
+            this.lbLog.Size = new System.Drawing.Size(776, 364);
             this.lbLog.TabIndex = 1;
             // 
-            // lblStatus
+            // aaa
             // 
-            this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(131, 16);
-            this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(17, 15);
-            this.lblStatus.TabIndex = 2;
-            this.lblStatus.Text = "--";
+            this.aaa.AutoSize = true;
+            this.aaa.Location = new System.Drawing.Point(131, 16);
+            this.aaa.Name = "aaa";
+            this.aaa.Size = new System.Drawing.Size(80, 15);
+            this.aaa.TabIndex = 2;
+            this.aaa.Text = "SerDownload:";
             // 
             // button1
             // 
@@ -76,13 +80,54 @@ namespace Ardin.GSAManager
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // lblSerStatus
+            // 
+            this.lblSerStatus.AutoSize = true;
+            this.lblSerStatus.Location = new System.Drawing.Point(217, 16);
+            this.lblSerStatus.Name = "lblSerStatus";
+            this.lblSerStatus.Size = new System.Drawing.Size(13, 15);
+            this.lblSerStatus.TabIndex = 2;
+            this.lblSerStatus.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CBDownload:";
+            // 
+            // lblCbStatus
+            // 
+            this.lblCbStatus.AutoSize = true;
+            this.lblCbStatus.Location = new System.Drawing.Point(374, 16);
+            this.lblCbStatus.Name = "lblCbStatus";
+            this.lblCbStatus.Size = new System.Drawing.Size(13, 15);
+            this.lblCbStatus.TabIndex = 2;
+            this.lblCbStatus.Text = "0";
+            // 
+            // cbCBDownload
+            // 
+            this.cbCBDownload.AutoSize = true;
+            this.cbCBDownload.Location = new System.Drawing.Point(12, 46);
+            this.cbCBDownload.Name = "cbCBDownload";
+            this.cbCBDownload.Size = new System.Drawing.Size(98, 19);
+            this.cbCBDownload.TabIndex = 4;
+            this.cbCBDownload.Text = "Download CB";
+            this.cbCBDownload.UseVisualStyleBackColor = true;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbCBDownload);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblCbStatus);
+            this.Controls.Add(this.lblSerStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.aaa);
             this.Controls.Add(this.lbLog);
             this.Controls.Add(this.btnStart);
             this.Name = "frmMain";
@@ -96,8 +141,12 @@ namespace Ardin.GSAManager
 
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.ListBox lbLog;
-        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label aaa;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblSerStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCbStatus;
+        private System.Windows.Forms.CheckBox cbCBDownload;
     }
 }
 
--- Ardin.GSAManager/frmMain.cs
+++ Ardin.GSAManager/frmMain.cs
@@ -10,6 +10,7 @@ using System.Net;
 using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,6 +18,8 @@ namespace Ardin.GSAManager
 {
     public partial class frmMain : Form
     {
+        public bool SerDownloadCompleted { get; set; }
+        public bool CBDownloadCompleted { get; set; }
         public frmMain()
         {
             InitializeComponent();
@@ -39,9 +42,6 @@ namespace Ardin.GSAManager
                     cb.Kill();
                     Log("Cb killed");
                 }
-                Log("Try to run Cb");
-                Process.Start(@"C:\Program Files (x86)\GSA Captcha Breaker\GSA_CapBreak.exe");
-                Log("Cb started");
                 var serAppDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "GSA Search Engine Ranker");
                 if (Directory.Exists(serAppDataPath))
                 {
@@ -55,61 +55,119 @@ namespace Ardin.GSAManager
                     Log("Ser program files deleted");
                 }
 
-                Download("ser");
-            });
-        }
+                if (!cbCBDownload.Checked)
+                {
+                    Log("Try to run Cb");
+                    Process.Start(@"C:\Program Files (x86)\GSA Captcha Breaker\GSA_CapBreak.exe");
+                    Log("Cb started");
+                }
+                else
+                {
+                    var cbAppDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "GSA Captcha Breaker");
+                    if (Directory.Exists(cbAppDataPath))
+                    {
+                        Directory.Delete(cbAppDataPath, true);
+                        Log("CB app data files deleted");
+                    }
+                    var cbProgramfilesPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "GSA Captcha Breaker");
+                    if (Directory.Exists(cbProgramfilesPath))
+                    {
+                        Directory.Delete(cbProgramfilesPath, true);
+                        Log("CB program files deleted");
+                    }
+                }
 
-        private void Download(string app)
-        {
-            try
-            {
+                Download();
+                while (!SerDownloadCompleted || !CBDownloadCompleted)
+                {
+                    Thread.Sleep(1000);
+                }
+
+                System.IO.Compression.ZipFile.ExtractToDirectory(@"C:\GSA\ser_setup.zip", @"C:\GSA");
                 string zipFile = @"C:\GSA\ser_setup.zip";
                 if (File.Exists(zipFile))
                     File.Delete(zipFile);
-                string extractedFile = @"C:\GSA\ser_setup.exe";
-                if (File.Exists(extractedFile))
-                    File.Delete(extractedFile);
-                Log("Start downloading new version of Ser");
-                WebClient webClient = new WebClient();
-                webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
-                webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+                Log("New version of Ser " + (cbCBDownload.Checked ? "and CB " : "") + "downloaded and extracted");
+                ProcessStartInfo startInfo = new ProcessStartInfo()
+                {
+                    FileName = "C:\\GSA\\ser_setup.exe"
+                };
+                Process.Start(startInfo);
+                if (cbCBDownload.Checked)
+                {
+                    startInfo = new ProcessStartInfo()
+                    {
+                        FileName = "C:\\GSA\\captcha_breaker.exe"
+                    };
+                    Process.Start(startInfo);
+                }
+                if (MessageBox.Show("Compelete setup and then press okey", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+                {
+                    Process.Start(@"C:\Program Files (x86)\GSA Search Engine Ranker\Search_Engine_Ranker.exe");
+                    if (cbCBDownload.Checked)
+                        Process.Start(@"C:\Program Files (x86)\GSA Captcha Breaker\GSA_CapBreak.exe");
+                }
+                Log("Finish");
+            });
+        }
 
-                webClient.DownloadFileAsync(new Uri("https://www.gsa-online.de/download/ser_setup.zip"), @"C:\GSA\ser_setup.zip");
+        private void Download()
+        {
+            try
+            {
+                string serZipFile = @"C:\GSA\ser_setup.zip";
+                string cBExtractedFile = @"C:\GSA\captcha_breaker.exe";
+                string serExtractedFile = @"C:\GSA\ser_setup.exe";
+
+                if (File.Exists(serZipFile))
+                    File.Delete(serZipFile);
+                if (File.Exists(serExtractedFile))
+                    File.Delete(serExtractedFile);
+                if (cbCBDownload.Checked)
+                {
+                    if (File.Exists(cBExtractedFile))
+                        File.Delete(cBExtractedFile);
+                }
+                Log("Start downloading new version of Ser " + (cbCBDownload.Checked ? " and Cb" : ""));
+                WebClient webClientSer = new WebClient();
+                webClientSer.DownloadProgressChanged += WebClientSer_DownloadProgressChanged;
+                webClientSer.DownloadFileCompleted += WebClientSer_DownloadFileCompleted;
+                webClientSer.DownloadFileAsync(new Uri("https://www.gsa-online.de/download/ser_setup.zip"), serZipFile);
+
+                if (cbCBDownload.Checked)
+                {
+                    WebClient webClientCb = new WebClient();
+                    webClientCb.DownloadProgressChanged += WebClientSb_DownloadProgressChanged;
+                    webClientCb.DownloadFileCompleted += WebClientCb_DownloadFileCompleted;
+                    webClientCb.DownloadFileAsync(new Uri("https://www.gsa-online.de/download/captcha_breaker.exe"), cBExtractedFile);
+                }
+                else CBDownloadCompleted = true;
             }
             catch (Exception ex)
             {
                 Log("Download ser failed", ex);
             }
         }
-
-        private void WebClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        private void WebClientCb_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
         {
-            System.IO.Compression.ZipFile.ExtractToDirectory(@"C:\GSA\ser_setup.zip", @"C:\GSA");
-            string zipFile = @"C:\GSA\ser_setup.zip";
-            if (File.Exists(zipFile))
-                File.Delete(zipFile);
-            Log("New version of Ser downloaded and extracted");
-            ProcessStartInfo startInfo = new ProcessStartInfo()
-            {
-                FileName = "C:\\GSA\\ser_setup.exe"
-            };
-            Process.Start(startInfo);
-            if (MessageBox.Show("Compelete setup and then press okey", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
-            {
-                Log("Setup compeleted");
-                Log("Run ser");
-                Process.Start(@"C:\Program Files (x86)\GSA Search Engine Ranker\Search_Engine_Ranker.exe");
-
-                Log("Ser started , try to restore config files");
-            }
-            Log("Finish");
+            CBDownloadCompleted = true;
         }
-
-        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        private void WebClientSb_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
-            lblStatus.Invoke((MethodInvoker)delegate
+            lblCbStatus.Invoke((MethodInvoker)delegate
             {
-                lblStatus.Text = e.ProgressPercentage + "%";
+                lblCbStatus.Text = e.ProgressPercentage + "%";
+            });
+        }
+        private void WebClientSer_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            SerDownloadCompleted = true;
+        }
+        private void WebClientSer_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            lblSerStatus.Invoke((MethodInvoker)delegate
+            {
+                lblSerStatus.Text = e.ProgressPercentage + "%";
             });
         }
 
@@ -135,7 +193,6 @@ namespace Ardin.GSAManager
                 lbLog.Items.Insert(0, DateTime.Now.ToString() + "\t" + message);
             }
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             var hanle = (IntPtr)0x002C0752;
